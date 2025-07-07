83db2c640fef0cf0ef2824040ca96f59308f18dd
Javier Torres <javier.torresvalenz@gmail.com>
2025-07-06 23:23:13 -0400

    mejoras visuales, efectos y modales

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,33 @@
 $(document).ready(function () {
     $("#cubo1").on("mouseenter", function () {
         $("#cubo1").hide(1000),
-        $("#trail1").hide(1000)
+            $("#trail1").hide(1000)
     })
     $("#cubo1").on("mouseleave", function () {
         $("#cubo1").show(1000),
-        $("#trail1").show(1000)
+            $("#trail1").show(1000)
     })
 
     $("#cubo2").on("mouseenter", function () {
         $("#cubo2").hide(1000),
-        $("#trail2").hide(1000)
+            $("#trail2").hide(1000)
     })
     $("#cubo2").on("mouseleave", function () {
         $("#cubo2").show(1000),
-        $("#trail2").show(1000)
+            $("#trail2").show(1000)
     })
+
+
+    $("#botonFrontEnd").click(function () {
+        $("#listaFrontend").slideToggle();
+    });
+
+    $("#botonBackEnd").click(function () {
+        $("#listaBackend").slideToggle();
+    });
+
+    $("#botonOtros").click(function () {
+        $("#listaOtros").slideToggle();
+    });
+    
 });
